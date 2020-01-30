b3776a1caac5fc9046e81bf5aadfdcce5e211121
sal'e chocho <malachocho01@gmail.com>
2020-01-31 01:00:33 +0200

    I think

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -12,17 +12,28 @@
 
 #include "../includes/get_next_line.h"
 
+static int		ft_readFile(l_list **list, int fd)
+{
+	char		buffer[BUFF_SIZE + 1];
+	int		result;
+	int		i;
+
+	while ((result = read(fd, buffer, BUFF_SIZE)) > 0)
+	{
+		buffer[result] = '\0';
+		ft_addtail(list, buffer, (size_t)result);
+		if ((i = ft_strichr(buffer, '\n')) > -1)
+			break ;
+	}
+	return (i);
+}
+
 static char		*ft_getline(l_list **list, int *i, int *j)
 {
 	char    temp_1[MAX_SIZE];
-	char	*temp_2;
-	char	*str;
-	size_t	length;
 
 	if (*list == NULL)
 		return (NULL);
-	str = (*list)->current->str;
-	length = (*list)->current->length;
 	ft_bzero(temp_1, 1);
 	if (*i > -1)
 	{
@@ -30,25 +41,40 @@ static char		*ft_getline(l_list **list, int *i, int *j)
 		*j = (BUFF_SIZE * (*j)) + *i;
 		if ((size_t)(*i) + 1 < (*list)->current->length)
 		{
-			temp_2 = ft_strsub(str, (*i) + 1, length - ((*i) + 1));
-			ft_strcleandel(&(*list)->current->str);
-			(*list)->current->str = temp_2;
-			(*list)->current->length = length - ((*i) + 1);
+			ft_strsplice(list, (*i + 1));
 			return (ft_strdup(temp_1));
 		}
 	}
 	else
-			ft_strcat(temp_1, (*list)->current->str);
+		ft_strcat(temp_1, (*list)->current->str);
 	ft_cleanher(list, 1);
 	return (ft_strdup(temp_1));
 }
 
-static int		ft_concatbuffers(l_list **list, char **line, int i)
+static int		ft_concatbuffers(l_list **list, char **str, int *i, int *j)
 {
 	t_line	*current;
-	char	*temp;
-	char 	str[MAX_SIZE];
-	int		j;
+	char 	*temp;
+
+	current = (*list)->current;
+	if (*str == NULL)
+		*str = ft_strnew(0);
+	if (current->next == NULL || current->str[*i] == '\n')
+	{
+		temp = ft_getline(list, i, j);
+		ft_concatstr(str, temp);
+		ft_strcleandel(&temp);
+		return (1);
+	}
+	ft_concatstr(str, (*list)->current->str);
+	ft_cleanher(list, 0);
+	return (0);
+}
+
+static int		ft_scanbuffers(l_list **list, char **line, int i)
+{
+	char 	*str;
+	int	j;
 	
 	if (*list == NULL)
 		return 0;
@@ -56,61 +82,44 @@ static int		ft_concatbuffers(l_list **list, char **line, int i)
 	if ((*list)->current == NULL)
 		return 0;
 	j = 0;
-	ft_bzero(str, 1);
+	str = ft_strnew(0);
 	while ((*list)->current != NULL)
 	{
-		current = (*list)->current;
-		if (current->next == NULL || current->str[i] == '\n')
-		{
-			temp = ft_getline(list, &i, &j);
-			ft_strcat(str, temp);
-			ft_strcleandel(&temp);
+		if (ft_concatbuffers(list, &str, &i, &j))
 			break;
-		}
-			else
-				ft_strcat(str, (*list)->current->str);
-		ft_cleanher(list, 0);
 		j++;
 	}
 	if (str[0] != '\0' || i > -1)
 	{
 		*line = (i > -1) ? ft_strndup(str, j) : ft_strdup(str);
+		ft_strcleandel(&str);
 		return (1);
 	}
+	ft_strcleandel(&str);
 	return (0);
 }
 
 int			get_next_line(const int fd, char **line)
 {
-	int				result;
-	int				i;
-	char			buffer[BUFF_SIZE + 1];
-	static l_list	*temp;
+	int		i;
+	char		buffer[1];
+	static l_list	*list;
 
-	if (fd < 0 || read(fd, buffer, 0) < 0 || BUFF_SIZE < 0)
+	if (fd < 0 || read(fd, buffer, 0) < 0 || BUFF_SIZE <= 0)
 		return (-1);
-	if (temp == NULL)
+	if (list != NULL)
 	{
-		if (!(temp = (l_list *)malloc(sizeof(l_list))))
-			return (-1);
-		ft_initlist(&temp);
-	}
-	else
-	{
-		temp->current = temp->head;
-		while (temp->current != NULL)
+		list->current = list->head;
+		while (list->current != NULL)
 		{
-			if ((i = ft_strichr(temp->current->str, '\n')) > -1)
-				return ft_concatbuffers(&temp, line, i);
-			temp->current = temp->current->next;
+			if ((i = ft_strichr(list->current->str, '\n')) > -1)
+				return ft_scanbuffers(&list, line, i);
+			list->current = list->current->next;
 		}
+	} else {
+		list = (l_list *)malloc(sizeof(l_list));
+		ft_initlist(&list);
 	}
-	while ((result = read(fd, buffer, BUFF_SIZE)) > 0)
-	{
-		buffer[result] = '\0';
-		ft_addtail(&temp, buffer, (size_t)result);
-		if ((i = ft_strichr(buffer, '\n')) > -1)
-			break ;
-	}
-	return ft_concatbuffers(&temp, line, i);
+	i = ft_readFile(&list, fd);
+	return ft_scanbuffers(&list, line, i);
 }
